868beaf48a41e2a55d84772ea85c5b64c0937513
Osvaldo Holanda <osvaldolimah@gmail.com>
2023-11-02 20:54:55 -0300

    implementado o contador de vidas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -3,13 +3,15 @@ const state = {
         squares: document.querySelectorAll(".square"),
         enemy: document.querySelector(".enemy"),
         timeLeft: document.querySelector("#time-left"),
-        score: document.querySelector("#score")
+        score: document.querySelector("#score"),
+        life: document.querySelector("#life"),
     },
     values: {
-        gameVelocity: 1000,
+        gameVelocity: 800,
         hitPosition: 0,
         result: 0,
-        currentTime: 60,
+        currentTime: 10,
+        chance: 1,
     },
     actions: {
         timerId: setInterval(randomSquare, 1000),
@@ -25,7 +27,18 @@ function countDown() {
         clearInterval(state.actions.countDownTimerId);
         clearInterval(state.actions.timerId);
         alert(`Game Over! O seu resultado foi: ${state.values.result}`);
+        state.values.chance--;
+        state.view.life.textContent = state.values.chance;
+        if(state.values.chance === 0) {
+            return state.view.life.textContent === chance;
+        }
     }
+}
+
+function playSound(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}.mp3`);
+    audio.volume = 0.3;
+    audio.play();
 }
 
 function randomSquare() {
@@ -46,6 +59,8 @@ function addListenerHitbox() {
                 state.values.result++;
                 state.view.score.textContent = state.values.result;
                 state.values.hitPosition = null;
+                playSound("tiro");
+
             }
     }) });
 }
